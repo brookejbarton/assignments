7d7eac35cb3d1401a7e5f663179729adf7f4d9c6
root <root@DESKTOP-A766ILR.localdomain>
2022-01-18 23:27:48 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Aline Normoyle
+ * Date: 1/11/2022
+ * Print Hello World
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Too many people volunteer to carry the stool when its time to move the piano.\n");
+  return 0;
+}
