69c5afc7f0de3c48919a30a44648ff12ccd2285a
Brooke <brookejbarton@gmail.com>
2022-03-03 13:10:06 -0500

    start A06

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,73 @@
 #include <stdio.h>
 #include "read_ppm.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+:w:w
+char* convert_bit(unsigned char color, char *bits){
+  char odd = '1';
+  char even = '0';
+
+  if (color%2!=0){
+    strcat(bits, &odd);
+  } else {
+    strcat(bits, &even);
+  }
+
+  return bits;
+}
+
+char* bit_to_ASCII(char bits[72]){ //param will be char*
+  char binstr[8];
+  char *ascii = malloc(sizeof(char)*73);//*(sizeof(bits)/8)+1);
+  bool b = true;
+  int count = 0; 
+  int shift = 0;
+  
+ /* if (sizeof(bits)%8!=0){
+    printf("ERROR: Not Binary");
+    exit(1);
+  }*/
+   
+  while (b == true){
+    for (int i = 0; i < 8; i++){
+      binstr[i] = bits[i+shift];
+    }
+    
+    for (int i = 0; i < 8; i++){
+      if (binstr[i] == 1){
+        count+=1;
+      }
+    }
+
+    if (count == 0){
+      return ascii;
+    }
+  
+    char c = strtol(binstr, (char **)NULL, 2);
+    strcat(ascii, &c);
+    count = 0;
+    shift+=8;
+  }
+  return ascii;
+}
 
 int main(int argc, char** argv) {
+  struct ppm_pixel *rgb;
+  int w = 0;
+  int h = 0;
+  
+  rgb = read_ppm(argv[1], &w, &h);
+  char *bits = malloc((sizeof(int)*sizeof(rgb))+1);
+//JUST CYCLES THORUGH 255 3 TIMES  
+    for (int i = 0; i < 3; i++){
+      convert_bit(rgb->red, bits);
+      convert_bit(rgb->green, bits);
+      convert_bit(rgb->blue, bits);
+    }
+  char practice[72] = {0,0,1,1,0,0,0,1,0,0,1,1,0,1,1,1,0,0,1,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0};
+  char *ascii = bit_to_ASCII(practice);
+  printf("%s", ascii);
   return 0;
 }
 
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -3,13 +3,35 @@
 #include <string.h>
 #include "read_ppm.h"
 
-// TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
-// array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+   struct ppm_pixel *ret_pix;
+   char buffer[128];
 
-  return NULL;
-}
+
+   FILE *fp = fopen(filename, "rb");
+
+
+   fgets(buffer, 128, fp);
+   if (buffer[0] != 'P' || buffer[1] != '6'){
+     printf("BAD");
+     exit(1);
+   }
+
+   fgets(buffer, 128, fp);
+   if (buffer[0] == '#'){
+     while (buffer[0] == '#'){
+       fgets(buffer, 128, fp);
+     }
+   }
+
+   sscanf(buffer, "%d %d", w, h);
+   ret_pix = malloc(sizeof(struct ppm_pixel *)*(*w)*(*h));
+
+   fread(ret_pix, sizeof(struct ppm_pixel), (*w)*(*h), fp);
+   fclose(fp);
+
+   return ret_pix;
+ }
 
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
