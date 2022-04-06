8795abdf9e508b4f594be36241436414d1095eda
Brooke <brookejbarton@gmail.com>
2022-04-06 15:48:14 -0400

    A09 update

--- A09/read_ppm.c
+++ A09/read_ppm.c
@@ -4,13 +4,49 @@
 #include "read_ppm.h"
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  struct ppm_pixel *ret_pix;
+   char buffer[128];
 
-  // your code here
-  return NULL;
+
+   FILE *fp = fopen(filename, "rb");
+
+
+   fgets(buffer, 128, fp);
+   if (buffer[0] != 'P' || buffer[1] != '6'){
+     printf("BAD");
+     exit(1);
+   }
+
+   fgets(buffer, 128, fp);
+   if (buffer[0] == '#'){
+     while (buffer[0] == '#'){
+       fgets(buffer, 128, fp);
+     }
+   }
+
+   sscanf(buffer, "%d %d", w, h);
+   ret_pix = malloc(sizeof(struct ppm_pixel *)*(*w)*(*h));
+
+   fgets(buffer, 128, fp);
+   fread(ret_pix, sizeof(struct ppm_pixel), (*w)*(*h), fp);
+   fclose(fp);
+
+   return ret_pix;
 }
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  FILE *fp = fopen(filename, "wb");
+  if (fp == NULL){
+    printf("ERROR: unable to open file.\n");
+    exit(1);
+  }
+  
+  fprintf(fp, "P6\n");
+  fprintf(fp, "# comments\n");
+  fprintf(fp, "%d %d\n", w, h);
+  fprintf(fp, "%d\n", 255);
+  fwrite(pxs->colors, 6, 3*w*h, fp);
 
-  // your code here
+  fclose(fp);
 }
 
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -4,14 +4,20 @@
 #include <time.h>
 #include <sys/time.h>
 #include "read_ppm.h"
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-  int size = 480;
+  int size = 100;
   float xmin = -2.0;
   float xmax = 0.47;
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
+
+  double timer;
+  struct timeval tstart, tend;
+
+  gettimeofday(&tstart, NULL);
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
@@ -28,68 +34,87 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
   // generate pallet
-  int w = size; //abs(xmax-xmin);
-  int h = size ; //abs(ymax-ymin);
-
+  srand(time(0));
   struct ppm_pixel *pallet = calloc(maxIterations, sizeof(struct ppm_pixel)); //read_ppm(argv[1], &w, &h); 
   
-  srand(time(0));
-
+  int basered = rand() % 255;
+  int basegreen = rand() % 255;
+  int baseblue = rand() % 255;
   for (int i = 0; i < maxIterations; i++){
-    pallet[i].red = rand() % 255;
-    pallet[i].green = rand() % 255;
-    pallet[i].blue = rand() % 255;
-  //  printf("pallet: r %d, g %d, b %d", pallet[i].red, pallet[i].green, pallet[i].blue);
+    pallet[i].red = basered + rand() % 100 - 50; //rand() % 255;
+    pallet[i].green = basegreen + rand() % 100 - 50; //rand() % 255;
+    pallet[i].blue = baseblue + rand() % 100 - 50; //rand() % 255;
+ // printf("pallet: r %d, g %d, b %d\n", pallet[i].red, pallet[i].green, pallet[i].blue);
   }
+
+  struct ppm_pixel *to_pass = malloc(sizeof(struct ppm_pixel)*(size)*(size));
+  
+
+  // compute image
+  for (int i = 0; i < size; i++){ 
+    for (int j = 0; j < size; j++){
+      float xfrac = (float)(j) / size;
+      float yfrac = (float)(i) / size;
+    //  printf("xfrac: %f, yfrac: %f\n", xfrac, yfrac);
+
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+
+      float x = 0;
+      float y = 0;
+      int iter = 0; 
+    
+      while (iter < maxIterations && x*x + y*y < 2*2){
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+       // printf("Y: %f, X: %f, ITER: %d\n", y, x, iter);
+      }
+
+      if (iter < maxIterations){ // escaped
+        to_pass[i*size+j].red = pallet[iter].red;
+        to_pass[i*size+j].green = pallet[iter].green;
+        to_pass[i*size+j].blue = pallet[iter].blue;
+     
+       //printf("color: %d, %d, %d", to_pass[i*size+j].red, to_pass[i*size+j].green, to_pass[i*size+j].blue);
+      } else { 
+        to_pass[i*size+j].red = 0;
+        to_pass[i*size+j].green = 0;
+        to_pass[i*size+j].blue = 0;
+
+     //printf("\nBLACK\n");
+     } 
+    }
+  }
+
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size, timer);
   
   int timestamp = time(0);
-  char name1[] = "data/mendelbrot-.png";
-  //strcat(name1, size);
-  //strcat(name1, "-");
-  //strcat(name1, timestamp);
-  //strcat(name1, ".png");
+  char name1[] = "mandelbrot-";
+  char sizestr[20];
+  sprintf(sizestr, "%d", size);
+  strcat(name1, sizestr);
+  strcat(name1,"-");
+  char timestr[20];
+  sprintf(timestr, "%d", timestamp);
+  strcat(name1, timestr);
+  strcat(name1, ".ppm");
   const char *filename = name1;
 
   if (filename == NULL){
     printf("Unable to create file.\n");
     exit(1);
   }
-  // compute image
-  int iter = 0;
-  for (int i = 0; i < h; i++){
-    for (int j = 0; j < w; j++){
-      int xfrac = j / size;
-      int yfrac = i / size;
-      int x0 = xmin + xfrac * (xmax - xmin);
-      int y0 = ymin + yfrac * (ymax - ymin);
+  printf("Writing file: %s\n", name1);
 
-      int x = 0;
-      int y = 0;
-      
-      struct ppm_pixel *color = calloc(1, sizeof(struct ppm_pixel));
+  write_ppm(filename, to_pass, size, size);
 
-      while (iter < maxIterations && x*x + y*y < 2*2){
-        int xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        
-
-        if (iter < maxIterations){ // escaped
-          color->red = pallet[iter].red;
-          color->green = pallet[iter].green;
-          color->blue = pallet[iter].blue;
-        } else { //i don't understand how this is meant to be reached, iter will always be less than MAX
-          color->red = 0;
-          color->green = 0;
-          color->blue = 0; //black
-        }
-
-      //  printf("r %d, g %d, b%d || ", color->red, color->green, color->blue);
-        iter++;
-       // write_ppm(filename, color, w, h); //write color to image at location (row,col)
-      } 
-    }
-  }
+  free(to_pass);
+  to_pass = NULL;
+  free(pallet);
+  pallet = NULL;
 }
